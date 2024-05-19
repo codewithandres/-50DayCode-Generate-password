895e9bf4da343961627230fd5ca2e8c2a9e5afa1
Dize dev <axiozweb2022@gmail.com>
2024-05-19 12:15:04 -0500

    creamos la funcionde generar contraseña

--- js/app.js
+++ js/app.js
@@ -1,10 +1,64 @@
 
-const lenghtSlider = document.querySelector('.pass-length input');
+const lenghtSlider = document.querySelector('.pass-length input'),
+    generateBtn = document.querySelector('.generate-btn'),
+    passwordInput = document.querySelector('.input-box input'),
+    options = document.querySelectorAll('.options input');
+
+const characters = { // object of letters, numbers & symbols
+    lowercase: "abcdefghijklmnopqrstuvwxyz",
+    uppercase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+    numbers: "0123456789",
+    symbols: "^!$%&|[](){}:;.,*+-#@<>~"
+};
+
+const generatePassword = () => {
+    let staticPassword = '',
+        randownPassword = '',
+        excludeDuplicate = false,
+        passLength = lenghtSlider.value;
+
+    [...options].map(option => {
+
+        if (option.checked) {
+
+            if (option.id !== 'exc-duplicate' && option.id !== 'spaces') {
+
+                staticPassword += characters[option.id]
+            } else if (option.id === 'spaces') {
+
+                staticPassword += `  ${staticPassword}  `;
+            } else {
+                excludeDuplicate = true;
+            };
+        };
+    });
+
+    for (let i = 0; i < passLength; i++) {
+
+        let randomChart = staticPassword[Math.floor(Math.random() * staticPassword.length)];
+
+        if (excludeDuplicate) {
+
+            !randownPassword.includes(randomChart) || randomChart == ' '
+                ?
+                randownPassword += randomChart
+                :
+                i--;
+
+        } else {
+            randownPassword += randomChart;
+        };
+    };
+    passwordInput.value = randownPassword
+};
 
 const updateSlider = () => {
+
     document.querySelector('.pass-length span').textContent = lenghtSlider.value;
+    generatePassword();
 };
 
 updateSlider();
 
 lenghtSlider.addEventListener('input', updateSlider);
+generateBtn.addEventListener('click', generatePassword);
